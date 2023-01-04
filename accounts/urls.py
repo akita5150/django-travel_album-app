bb1f408cdeec914596d76d604ee24c77605ffea3
akita <akitakita5150@gmail.com>
2023-01-04 23:12:34 +0900

    会員登録機能実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from django.contrib.auth.views import LogoutView
-from accounts.views import MyLoginView
+from accounts.views import MyLoginView, SignUpView
 
 
 urlpatterns = [
     path('login/', MyLoginView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]

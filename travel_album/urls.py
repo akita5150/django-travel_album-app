1ac71679e682ddca1301689fd4e3753db7d76b02
akita <akitakita5150@gmail.com>
2023-01-06 19:26:53 +0900

    Album_create作成

--- travel_album/urls.py
+++ travel_album/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from travel_album.views import Diary_listView, Diary_DetailView, Album_listView, Photo_listView, Diary_CreateView
+from travel_album.views import Diary_listView, Diary_DetailView, Album_listView, Photo_listView, Diary_CreateView, Album_CreateView
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('diary/<int:pk>', Diary_DetailView.as_view(), name='diary-detail'),
     path('diary/create', Diary_CreateView.as_view(), name='diary-create'),
     path('diary/<int:diary_pk>/album', Album_listView.as_view(), name='album-list'),
+    path('diary/<int:diary_pk>/album/create', Album_CreateView.as_view(), name='album-create'),
     path('diary/<int:album_diary_pk>/album/<int:album_pk>/photo', Photo_listView.as_view(), name='photo-list'),
 ]
--- travel_album/views.py
+++ travel_album/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from travel_album.models import Diary, Album, Photo, Prefectures
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 
 # Create your views here.
@@ -26,6 +26,11 @@ class Diary_CreateView(CreateView):
         initial['user'] = self.request.user   
         return initial
     success_url = reverse_lazy('diary-list')
+    def get_success_url(self):
+        # 新規データのID取得
+        id = self.object.id
+        return reverse("diary-detail", kwargs={"pk": id})
+    
 
 class Album_listView(ListView):
     model = Album
@@ -36,7 +41,17 @@ class Album_listView(ListView):
         Album_list = Album.objects.filter(diary_id=self.kwargs['diary_pk']) 
         context["Albums"] = Album_list
         return context
-        
+
+class Album_CreateView(CreateView):
+    model = Album
+    template_name = 'travel_album/album_create.html'
+    fields = ['diary', 'location']
+    def get_initial(self, **kwargs):
+        initial = super().get_initial()
+        Album_list = Diary.objects.filter(id=self.kwargs['diary_pk'])
+        initial['diary'] = Album_list[0]
+        return initial
+    success_url = reverse_lazy('diary-list')
 
 class Photo_listView(ListView):
     model = Photo

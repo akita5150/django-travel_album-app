09af587c91a502ce07f7d2604e1f0ebaeb5ea45c
akita <akitakita5150@gmail.com>
2023-03-11 02:53:00 +0900

    フォロー中リスト作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from django.contrib.auth.views import LogoutView
-from accounts.views import MyLoginView, SignUpView
+from accounts.views import MyLoginView, SignUpView, Following_DetailView
 
 
 urlpatterns = [
     path('login/', MyLoginView.as_view(), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('<int:pk>/following/', Following_DetailView.as_view(), name='following-list'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
+from accounts.models import User_information
+from django.contrib.auth.models import User
 
 # Create your views here.
 class MyLoginView(LoginView):
@@ -13,4 +15,16 @@ class SignUpView(CreateView):
     template_name = 'accounts/signup.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('diary-list')
+
+class Following_DetailView(DetailView):
+    model = User
+    template_name = 'accounts/following_list.html'
+    context_object_name = 'User'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        information = User_information.objects.get(user_id=self.kwargs['pk'])
+        context['information'] = information
+        return context
+        
+    
     

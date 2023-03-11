32e2c81f4314fc7d14de0526ff5e52e5464e784c
akita <akitakita5150@gmail.com>
2023-03-12 04:07:57 +0900

    ユーザーページ作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from django.contrib.auth.views import LogoutView
-from accounts.views import MyLoginView, SignUpView, Following_DetailView
+from accounts.views import MyLoginView, SignUpView, Following_DetailView, UserPage_View
 
 
 urlpatterns = [
     path('login/', MyLoginView.as_view(), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('<int:pk>/userpage/', UserPage_View.as_view(), name='userpage'),
     path('<int:pk>/following/', Following_DetailView.as_view(), name='following-list'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
 from accounts.models import User_information
+from travel_album.models import Diary
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -15,6 +16,20 @@ class SignUpView(CreateView):
     template_name = 'accounts/signup.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('diary-list')
+
+class UserPage_View(DetailView):
+    model = User
+    template_name = 'accounts/userpage.html'
+    context_object_name = 'User'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_list = Diary.objects.filter(user_id=self.kwargs['pk'],is_publish=True)
+        # user_information = User_information.objects.get(user_id=self.kwargs['pk'])
+        login_user_information = User_information.objects.get(user_id=self.request.user)
+        context['posts'] = post_list
+        # context['user_information'] = user_information
+        context['login_user_information'] = login_user_information
+        return context
 
 class Following_DetailView(DetailView):
     model = User
